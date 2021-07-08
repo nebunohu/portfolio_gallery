0985ca342f4631a69adb4f45bc90441c656cb1be
Barashkov <nebunohu@gmail.com>
2021-07-08 22:40:36 +0300

    Intermediate commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './style.scss';
 import './images/eye.png';
 import './images/tarot.png';
+import './images/mudrost.png';
+import './images/psychosomasys.png';
+import './images/devka-bez-ruki.png';
