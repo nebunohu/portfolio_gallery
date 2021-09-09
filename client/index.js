1116ba515c17107c6b6f353ed6b2d9c5e210429a
Barashkov <nebunohu@gmail.com>
2021-09-09 23:33:27 +0300

    Intermediate commit

--- client/index.js
+++ client/index.js
@@ -5,5 +5,7 @@ import './images/tarot.png';
 import './images/mudrost.png';
 import './images/psychosomasys.png';
 import './images/devka-bez-ruki.png';
+import './images/phone.png';
+import './images/lets_collaborate.svg';
 
 //fetch('http://localhost:3000')
