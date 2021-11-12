4649bd3e9f88056b899f126c421afab2798a26c4
Barashkov <nebunohu@gmail.com>
2021-11-12 17:15:52 +0300

    Intermediate commit

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -1,22 +1,63 @@
-import React, { Component } from "react";
+import React from "react";
 
-class LoginForm extends Component {
-  render() {
-    return (
-      <div>
-        <form className="login-form-wrapper">
-          <label className="login-form-wrapper__login-label" htmlFor="login" />Login
-          <input className="login-form-wrapper__input" type="text" id="login" name="login" placeholder="Login" />
-          <label className="login-form-wrapper__login-label" htmlFor="password" />Password
-          <input className="login-form-wrapper__input"type="password" id="password" name="password" placeholder="Password" />
-          <input className="login-form-wrapper__submit-button" type="submit" value="Sign in" />
-        </form>
-      </div>
-    );
-  }
+function LoginForm() {
+  const loginHandler = async function (event) {
+    event.preventDefault();
+    const login = event.target[0].value;
+    const password = event.target[1].value;
+  
+    try {
+      
+      let body = { login: login, password: password };
+      body = JSON.stringify(body);
+      let headers = new Headers({ "content-type": "application/json" });
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        body,
+        mode: "cors",
+        headers,
+      });
+      if (!response.ok) {
+        throw new Error("Ответ сети был не ок.");
+      } else {
+        //pasteHtmlTemplate();
+        // Получение изображения с сервера по адресу http://localhost:3000/static/img.jpg
+        /*body = { info: "image" };
+        const getMethodResponse = await fetch(
+          "http://localhost:3000/static/img.jpg",
+          { method: "GET", mode: "cors", headers }
+        )
+          .then((getMethodResponse) => getMethodResponse.blob())
+          .then(function (myBlob) {
+            let objectURL = URL.createObjectURL(myBlob);
+            let myImage = document.querySelector("img");
+            myImage.src = objectURL;
+          });
+        if (!getMethodResponse.ok) {
+          throw new Error("Ответ сети был не ок.");
+        }*/
+      }
+      
+    } catch (error) {
+      console.log("Возникла проблема с вашим fetch запросом: ", error.message);
+    }
+  };
+  
+  return (
+    <div>
+      <form className="login-form-wrapper" onSubmit={loginHandler}>
+        <label className="login-form-wrapper__login-label" htmlFor="login" />Login
+        <input className="login-form-wrapper__input" type="text" id="login" name="login" placeholder="Login" />
+        <label className="login-form-wrapper__login-label" htmlFor="password" />Password
+        <input className="login-form-wrapper__input"type="password" id="password" name="password" placeholder="Password" />
+        <input className="login-form-wrapper__submit-button" type="submit" value="Sign in" />
+      </form>
+    </div>
+  );
+  
 }
 
-class AdminComponent extends Component {
+class AdminComponent extends React.Component {
   render() {
     return (
       <LoginForm />
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -21,7 +21,7 @@ function App() {
   React.useEffect(() => {
     if (window.location.pathname === "/admin") setState({...state, isAdminPage: true});
     else setState({...state, isAdminPage: false});
-  }, [])
+  }, []);
 
   return (
     
@@ -40,70 +40,3 @@ function App() {
 }
 
 export default App;
-//import { AdminDashboard } from "./adminDashboard.jsx";
-
-//export let APP = {} || APP;
-
-/*APP.start = function () {
-  let upButton = document.querySelector(".up") || null;  
-
-  if (window.location.pathname === "/") {
-    APP.render();
-    //upButton.addEventListener("click", APP.backToTop);
-  } else if (window.location.pathname === "/admin/admin.html") {
-    ReactDOM.render(<AdminComponent />, document.querySelector('#root'));
-    let loginForm = document.querySelector(".login-form-wrapper") || null;
-    loginForm.addEventListener("submit", APP.loginHandler);
-  }
-};
-
-APP.backToTop = function () {
-  if (window.pageYOffset > 0) {
-    window.scrollBy(0, -30);
-    setTimeout(APP.backToTop, 0);
-  }
-};*/
-
-/*APP.loginHandler = async function (event) {
-  event.preventDefault();
-  const login = this[0].value;
-  const password = this[1].value;
-
-  try {
-    
-    let body = { login: login, password: password };
-    body = JSON.stringify(body);
-    let headers = new Headers({ "content-type": "application/json" });
-    const response = await fetch("http://localhost:3000/auth/login", {
-      method: "POST",
-      body,
-      mode: "cors",
-      headers,
-    });
-    if (!response.ok) {
-      throw new Error("Ответ сети был не ок.");
-    } else {
-      pasteHtmlTemplate();*/
-      // Получение изображения с сервера по адресу http://localhost:3000/static/img.jpg
-      /*body = { info: "image" };
-      const getMethodResponse = await fetch(
-        "http://localhost:3000/static/img.jpg",
-        { method: "GET", mode: "cors", headers }
-      )
-        .then((getMethodResponse) => getMethodResponse.blob())
-        .then(function (myBlob) {
-          let objectURL = URL.createObjectURL(myBlob);
-          let myImage = document.querySelector("img");
-          myImage.src = objectURL;
-        });
-      if (!getMethodResponse.ok) {
-        throw new Error("Ответ сети был не ок.");
-      }*/
-/*    }
-    
-  } catch (error) {
-    console.log("Возникла проблема с вашим fetch запросом: ", error.message);
-  }
-};*/
-
-
